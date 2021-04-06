aeea68067ffa5fe09cb5a02c233fe3c854cc4817
Reece Kidd <reecekidd95@gmail.com>
2021-04-06 15:07:55 +0100

    refactor: generated type defintions

--- app/next-env.d.ts
+++ app/next-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module '*.graphqls' {
+  import { Document } from 'graphql'
+  export default typeof DocumentNode
+}
+
+declare module '*.yml'
